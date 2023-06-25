996ddf6b32b609790e8d0128f553213ca250d697
Piotr Gamorski <pgamorski@wp.pl>
2023-06-25 10:32:26 +0200

    EventModel created

---
+++ CQRS-ES/CQRS.Core/Events/EventModel.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CQRS.Core.Events
+{
+    public class EventModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; init; } = null!;
+        public DateTime TimeStamp { get; init; }
+        public Guid AggregateIdentifier { get; init; }
+        public string AggregateType { get; init; } = null!;
+        public int Version { get; init; }
+        public string EventType { get; init; } = null!;
+        public BaseEvent EventData { get; init; } = null!;
+    }
+}
